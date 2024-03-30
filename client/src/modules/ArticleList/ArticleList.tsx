c1ae2f6740b85579e87b2f8793c9558b7775a7d3
dmytro-chushko <dmytro.chushko@gmail.com>
2024-03-30 14:17:19 +0200

    Add pagination

--- client/src/modules/ArticleList/ArticleList.tsx
+++ client/src/modules/ArticleList/ArticleList.tsx
@@ -1,8 +1,15 @@
-import { Grid } from '@mui/material';
+import { Grid, Pagination, Typography } from '@mui/material';
 import { Children, ReactElement, cloneElement, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useGetAllArticlesQuery } from 'src/redux/api/article.api';
 import { useSetLoaderStatus } from 'src/redux/reducers/loader';
-import { useGetSearch, useGetSort } from 'src/redux/reducers/searchParams';
+import {
+  useGetLimit,
+  useGetPage,
+  useGetSearch,
+  useGetSort,
+  useSetPage,
+} from 'src/redux/reducers/searchParams';
 
 import { IArticleRetrived } from 'src/types/api';
 import { ArticleEvent } from 'src/utils/consts';
@@ -13,10 +20,16 @@ interface IArticleListProps {
 }
 
 export const ArticleList = ({ children }: IArticleListProps) => {
+  const { t } = useTranslation();
   const setLoaderStatus = useSetLoaderStatus();
   const search = useGetSearch();
   const sort = useGetSort();
-  const queryParams = `${search && `search=${search}&`}sort=${sort}`;
+  const page = useGetPage();
+  const limit = useGetLimit();
+  const setPage = useSetPage();
+  const queryParams = `${
+    search && `search=${search}&`
+  }sort=${sort}&page=${page}&limit=${limit}`;
   const { data, isLoading, refetch } = useGetAllArticlesQuery(queryParams);
 
   const renderChildren = (article: IArticleRetrived) => {
@@ -27,10 +40,22 @@ export const ArticleList = ({ children }: IArticleListProps) => {
     });
   };
 
+  const handleChangePage = (
+    _event: React.ChangeEvent<unknown>,
+    page: number,
+  ) => {
+    setPage(page);
+  };
+
   useEffect(() => {
     setLoaderStatus(isLoading);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isLoading]);
+
+  useEffect(() => {
+    data && data.articles.length === 0 && setPage(1);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
 
   useEffect(() => {
     const handleRefetchArticle = () => refetch();
@@ -43,13 +68,44 @@ export const ArticleList = ({ children }: IArticleListProps) => {
   }, [refetch]);
 
   return (
-    <Grid component="ul" container rowSpacing={4}>
-      {data &&
-        data.articles.map(article => (
-          <Grid key={article.id} component="li" item xs={12}>
-            {renderChildren(article)}
+    <Grid container direction="column" spacing={4} alignItems="center">
+      {data && data.articles.length > 0 && (
+        <Grid item>
+          <Pagination
+            count={data.totalPages}
+            page={page}
+            onChange={handleChangePage}
+            variant="outlined"
+            shape="rounded"
+            color="primary"
+          />
+        </Grid>
+      )}
+      <Grid component="ul" item container rowSpacing={4}>
+        {data && data.articles.length > 0 ? (
+          data.articles.map(article => (
+            <Grid key={article.id} component="li" item xs={12}>
+              {renderChildren(article)}
+            </Grid>
+          ))
+        ) : (
+          <Grid item>
+            <Typography variant="h3">{t('notFound')}</Typography>
           </Grid>
-        ))}
+        )}
+      </Grid>
+      {data && data.articles.length > 0 && (
+        <Grid item>
+          <Pagination
+            count={data.totalPages}
+            page={page}
+            onChange={handleChangePage}
+            variant="outlined"
+            shape="rounded"
+            color="primary"
+          />
+        </Grid>
+      )}
     </Grid>
   );
 };
